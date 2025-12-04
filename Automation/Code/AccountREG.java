98b870d468b49a8e6f35c9ec92c07d29e1c9cac1
Light-NHU <weirdhippofather@gmail.com>
2025-12-04 19:19:35 +0200

    Add files via upload

--- Automation/Code/AccountREG.java
+++ Automation/Code/AccountREG.java
@@ -28,16 +28,130 @@ public class AccountREG {
     }
 
     @Test
-    public static void goToHistory(){
+    public static void goToHistoryID(){
 
         WebDriver drive = new ChromeDriver() ;
         System.setProperty("webdriver.chrome.driver","C:\\Users\\yucef\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         drive.manage().window().maximize();
         drive.get("https://parabank.parasoft.com/parabank/index.htm");
 
+        By username_loc_path = By.xpath("//input[@name=\'username\']");//paste the link for the xpath selected
+        By loginBtn = By.xpath("//input[@type='submit']");
+        By password_bar = By.xpath("//input[@type='password']");
+
+        WebElement word = drive.findElement(username_loc_path);
+        WebElement btn = drive.findElement(loginBtn);
+        WebElement passwordpar = drive.findElement(password_bar);
+        word.sendKeys("lightss");
+        passwordpar.sendKeys("123");
+        btn.click();
+
+
+
         By transferHistoryL = By.xpath("//a[contains(text(),'Find Transactions')]");
         WebElement transferHistorybtn = drive.findElement(transferHistoryL);
         transferHistorybtn.click();
+
+
+        //ID
+        By transferfunds_IDh = By.xpath("//input[@id='transactionId']");
+        By transferfunds_IDbh = By.xpath("//button[@id='findById']");
+        WebElement transferfunds_IDH = drive.findElement(transferfunds_IDh);
+        WebElement transferfunds_IDBH = drive.findElement(transferfunds_IDbh);
+
+
+        //the amount changes based on what the correct value is
+        transferfunds_IDH.sendKeys("13445");
+        transferfunds_IDBH.click();
+
+
+
+    }
+
+
+
+
+    @Test
+    public static void goToHistoryAmount() throws InterruptedException{
+
+        WebDriver drive = new ChromeDriver() ;
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\yucef\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        drive.manage().window().maximize();
+        drive.get("https://parabank.parasoft.com/parabank/index.htm");
+
+        By username_loc_path = By.xpath("//input[@name=\'username\']");//paste the link for the xpath selected
+        By loginBtn = By.xpath("//input[@type='submit']");
+        By password_bar = By.xpath("//input[@type='password']");
+
+        WebElement word = drive.findElement(username_loc_path);
+        WebElement btn = drive.findElement(loginBtn);
+        WebElement passwordpar = drive.findElement(password_bar);
+        word.sendKeys("lightss");
+        passwordpar.sendKeys("123");
+        btn.click();
+
+
+
+        By transferHistoryL = By.xpath("//a[contains(text(),'Find Transactions')]");
+        WebElement transferHistorybtn = drive.findElement(transferHistoryL);
+        transferHistorybtn.click();
+
+
+        //Amount
+        By transferfunds_Amounth = By.xpath("//input[@id='amount']");
+        By transferfunds_Amountb = By.xpath("//button[@id='findByAmount']");
+
+        WebElement transferfunds_AmountH = drive.findElement(transferfunds_Amounth);
+        WebElement transferfunds_AmountB = drive.findElement(transferfunds_Amountb);
+
+        transferfunds_AmountH.sendKeys("0");
+        Thread.sleep(3000);
+
+        transferfunds_AmountB.click();
+
+
+
+    }
+
+
+
+    @Test
+    public static void goToHistoryDate() throws InterruptedException  {
+
+        WebDriver drive = new ChromeDriver() ;
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\yucef\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        drive.manage().window().maximize();
+        drive.get("https://parabank.parasoft.com/parabank/index.htm");
+
+        By username_loc_path = By.xpath("//input[@name=\'username\']");//paste the link for the xpath selected
+        By loginBtn = By.xpath("//input[@type='submit']");
+        By password_bar = By.xpath("//input[@type='password']");
+
+        WebElement word = drive.findElement(username_loc_path);
+        WebElement btn = drive.findElement(loginBtn);
+        WebElement passwordpar = drive.findElement(password_bar);
+        word.sendKeys("lightss");
+        passwordpar.sendKeys("123");
+        btn.click();
+
+
+
+        By transferHistoryL = By.xpath("//a[contains(text(),'Find Transactions')]");
+        WebElement transferHistorybtn = drive.findElement(transferHistoryL);
+        transferHistorybtn.click();
+
+        Thread.sleep(3000);
+
+        //date
+        By transferfunds_Dateh = By.xpath("//input[@id='transactionDate']");
+        By transferfunds_Datebh = By.xpath("//button[@id='findByDate']");
+
+        WebElement transferfunds_DateH = drive.findElement(transferfunds_Dateh);
+        WebElement transferfunds_DateBH = drive.findElement(transferfunds_Datebh);
+
+        transferfunds_DateH.sendKeys("11-11-2024");
+        transferfunds_DateBH.click();
+
 
 
     }
